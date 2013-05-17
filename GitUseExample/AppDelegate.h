2ca8db4e362b088ac3ce5123e1a2d764e83d44fc
George <b4n4n4m0nt4n4@gmail.com>
2013-05-17 11:29:57 -0700

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Lion Rawr on 5/17/13.
+//  Copyright (c) 2013 Kuawa Road. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Lion Rawr on 5/17/13.
+//  Copyright (c) 2013 Kuawa Road. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
